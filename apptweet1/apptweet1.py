43a11b4f4b4f67e0bf451b27383e9a27d425199d
jmmay <josem571may@gmail.com>
2018-09-13 21:53:44 -0500

    inicial

---
+++ apptweet1/apptweet1.py
@@ -0,0 +1,35 @@
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+
+class MyListener(StreamListener):
+	
+	def on_data(self,data):
+		try:
+			with open('Tweets.json','a') as f:
+				f.write(data)
+				return True
+		except BaseException as e:
+	    	print("error en el dato:%s" % str(e))
+		return True
+
+	def on_error(self, status):
+		print(status)
+		return True
+
+consumer_key =""
+consumer_secret =""
+access_token =""
+access_secret =""
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
+
+
+	
